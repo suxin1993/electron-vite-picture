3c21144e4b3d3b0556b9aea2910afdce76a165d6
subin <18565641627@163.com>
2021-10-20 21:27:20 +0800

    [feat]: 修复压缩图片

--- src/renderer/work/compressionImageJpg.js
+++ src/renderer/work/compressionImageJpg.js
@@ -1,17 +1,49 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-14 11:00:11
+ * @LastEditTime: 2021-10-20 21:21:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /electron-vite-picture/src/renderer/work/compressionImageJpg.js
+ */
+// work 进程
+// const { writeFileAsync, readFile } = require('./src/renderer/utils/node-operate-folder.js')
+const { writeFileAsync, readFile } = require('../utils/node-operate-folder')
+const resizeOptimizeImages = require('resize-optimize-images');
 
 
-const { writeFileAsync, readFile } = require('./src/renderer/utils/node-operate-folder.js')
-const images = require("images")
 
-// 图片太大，容易崩溃，如何避免
-// 通过子进程运行
-self.addEventListener('message', async (e) => {
-    console.error('imageJpg')
-    let filePath = JSON.parse(e.data).filePath
-    console.error(JSON.parse(e.data).filePath)
-    images(filePath).save(filePath, { quality: 60 }) 
-    
-    setTimeout(() => {
-        self.close();
-    }, 2000)
-}, false);
+if (false) {
+    // 图片太大，容易崩溃，如何避免
+    // 通过子进程运行
+    self.addEventListener('message', async (e) => {
+        console.error('imageJpg')
+        let filePath = JSON.parse(e.data).filePath
+        console.error(JSON.parse(e.data).filePath)
+        images(filePath).save(filePath, { quality: 60 })
+
+        setTimeout(() => {
+            self.close();
+        }, 10000)
+    }, false);
+} else {
+    process.on('message', async function(m) {
+        console.log('message from parent: ' + JSON.stringify(m));
+        if (m.exit == 'exit') {
+            process.exit(process.pid)
+        } else if (m.filePath) {
+            const options = {
+                images: [m.filePath, m.filePath],
+                quality: 90
+            };
+            // Run the module.
+            await resizeOptimizeImages(options);
+
+            setTimeout(() => {
+                process.exit(process.pid)
+            }, 10000)
+        }
+    });
+    process.send({ from: 'child', pid: process.pid })
+
+}
